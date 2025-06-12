20b66d68b238dd1c691618b314e2b026e89b9215
Jerrywang993 <wang.tianye2@northeastern.edu>
2025-06-12 18:16:40 -0400

    feat: Add facing.h file
    - Add entity facing direction enum
    - Include four cardinal directions: left, right, up, down

---
+++ include/facing.h
@@ -0,0 +1,21 @@
+/**
+ * @file facing.h
+ * @brief 定义游戏中实体的朝向方向
+ */
+
+#ifndef _FACING_H_
+#define _FACING_H_
+
+/**
+ * @enum Facing
+ * @brief 表示实体可以朝向的四个基本方向
+ */
+enum Facing
+{
+     Left,
+     Right,
+     Up,
+     Down
+};
+
+#endif // !_FACING_H_
